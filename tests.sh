a0eadaed147e279e63f003fca49d23198ca17032
Jared Crain <jared.crain@wsu.edu>
2018-02-14 14:03:14 -0800

    Delete old scripts

--- tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Run the basic and quick automated tests
-# Need //bin since windows tries to convert /bin to a relative path
-docker exec wsufdockerwordpress_idonate-plugin_1 //bin/sh -c 'grunt serve'
-read -p 'Press [Enter] key to continue...'
